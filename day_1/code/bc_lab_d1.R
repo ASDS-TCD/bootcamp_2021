bbe92c230175e00562830bc575fcb85c37f4e873
Jeffrey Ziegler <zieglerjef@gmail.com>
2021-07-21 09:50:26 -0400

    Update bc_lab_d1.R

--- day_1/code/bc_lab_d1.R
+++ day_1/code/bc_lab_d1.R
@@ -259,6 +259,9 @@ logic
 # Word processing in Latex
 ##########################
 
+# https://www.overleaf.com/learn/latex/Free_online_introduction_to_LaTeX_(part_1)
+# https://www.overleaf.com/learn/latex/Learn_LaTeX_in_30_minutes
+
 ####################################
 # Part 3: 
 # Doing Problems Sets in R and Latex
